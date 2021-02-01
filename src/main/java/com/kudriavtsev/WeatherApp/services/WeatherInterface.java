e2f60fe145fb86115157a54ea83ea298d0aae56c
Kirill <kirill171095@gmail.com>
2021-02-01 14:23:02 +0300

    +Configuration class +Weather interface

---
+++ src/main/java/com/kudriavtsev/WeatherApp/services/WeatherInterface.java
@@ -0,0 +1,9 @@
+package com.kudriavtsev.WeatherApp.services;
+
+import com.kudriavtsev.WeatherApp.model.Weather;
+
+import java.util.List;
+
+public interface WeatherInterface {
+    List<Weather> gWeather();
+}
